3e36469275f6dca2e0f5795e2c4e373df28edb80
Misaki Koonce <misaki.koonce@gmail.com>
2020-01-24 22:37:31 -0800

    Update link styles.

--- PierresTreats/Models/PierresTreatsContext.cs
+++ PierresTreats/Models/PierresTreatsContext.cs
@@ -5,7 +5,7 @@ namespace PierresTreats.Models
 {
     public class PierresTreatsContext : IdentityDbContext<ApplicationUser>
     {
-        public DbSet<Flavor> Flavors { get; set; }
+        public virtual DbSet<Flavor> Flavors { get; set; }
         public DbSet<Treat> Treats { get; set; }
         public DbSet<FlavorTreat> FlavorTreats { get; set; }
 
