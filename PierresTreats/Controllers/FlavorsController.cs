fdebe7d5cea2ef3d20ee075175784c3bb37b6922
Misaki Koonce <misaki.koonce@gmail.com>
2020-01-30 22:02:34 -0800

    Delete extra lines and unused namespace.

--- PierresTreats/Controllers/FlavorsController.cs
+++ PierresTreats/Controllers/FlavorsController.cs
@@ -1,11 +1,9 @@
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using System.Threading.Tasks;
 using System.Security.Claims;
-using System.Collections.Generic;
 using System.Linq;
 using PierresTreats.Models;
 
--- PierresTreats/Controllers/TreatsController.cs
+++ PierresTreats/Controllers/TreatsController.cs
@@ -5,7 +5,6 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using System.Threading.Tasks;
 using System.Security.Claims;
-using System.Collections.Generic;
 using System.Linq;
 using PierresTreats.Models;
 
